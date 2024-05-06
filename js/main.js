982923f24130c1dac6830ee8fb5a84e81da22cf0
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-05-06 15:43:09 -0500

    fetch to github api ready and show response on interface

--- js/main.js
+++ js/main.js
@@ -3,7 +3,26 @@ const API = "https://api.github.com/users/";
 const app = Vue.createApp({
   data() {
     return {
-      message: 'Hello Vue'
+      search: null,
+      result: null,
+      errorFound: false,
+    }
+  },
+  methods: {
+    async doSearch() {
+      this.result = this.errorFound = null;
+      try {
+        const response = await fetch(API + this.search);
+        if(!response.ok) throw new Error(`user ${this.search} not found`)
+        const data = await response.json();
+        this.result = data;
+        console.log(data);
+
+      } catch (error) {
+        this.errorFound = error;
+      } finally {
+        this.search = null;
+      }
     }
   }
 })
